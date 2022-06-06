8b0009742a2819025f777044b8f1576a88ea6519
Dongwook Kim <dongwook227@gmail.com>
2022-06-06 05:51:22 -0400

    add `SignUp` basic view

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import './SignUp.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+}
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="signup">
+      <h5>Sign Up</h5>
+      <form className="signup-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>First Name</label>
+          <input {...register('firstName', { required: true, maxLength: 20 })} />
+          {errors.firstName && 'Fisrt name is required'}
+        </div>
+
+        <div className="form-group">
+          <label>Last Name</label>
+          <input {...register('lastName', { required: true, pattern: /^[A-Za-z]+$/i })} />
+          {errors.lastName && 'Last name is required'}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
